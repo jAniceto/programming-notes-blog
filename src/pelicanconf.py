fee93f7a88d4d274c25d9c36f0cc42fa39c43438
José Aniceto <jose.pedro.aniceto@gmail.com>
2018-07-29 23:59:50 +0100

    Small fixes

--- src/pelicanconf.py
+++ src/pelicanconf.py
@@ -4,7 +4,6 @@ from __future__ import unicode_literals
 
 AUTHOR = 'José Aniceto'
 SITENAME = 'Annotatio'
-SITEURL = ''
 BIO = 'A collection of codding notes and snippets from my journey of self taught programming.'
 # PROFILE_IMAGE = 'photoB&W2_square.jpg'
 
@@ -32,14 +31,13 @@ SOCIAL = (('Personal Page', 'http://joseaniceto.com'),
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
-RELATIVE_URLS = True
+# RELATIVE_URLS = True
 
 
 # Custom settings
 TYPOGRIFY = True
 THEME = 'themes/pelican-hyde-2'
 HYDE_THEME = 'theme-base-08'
-USE_FOLDER_AS_CATEGORY = True
 STATIC_PATHS = ['images']
 USE_FOLDER_AS_CATEGORY = True
 # ARTICLE_PATHS = ['']
